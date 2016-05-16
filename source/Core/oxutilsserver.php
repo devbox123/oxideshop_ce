13439f818252032b3a6bef901aafb0840b1dde54
Dev Box <devbox123@gmail.com>
2016-05-16 13:40:46 +0000

    fixing merging conflicts

--- source/Core/oxutilsserver.php
+++ source/Core/oxutilsserver.php
@@ -72,9 +72,9 @@ class oxUtilsServer extends oxSuperCfg
             // do NOT set cookies in php unit.
             return;
         }
-        $config = $this->getConfig();
-        //if shop runs in https only mode we can set secure flag to all cookies 
-        $blSecure = $blSecure || ($config->isSsl() && $config->getSslShopUrl() == $config->getShopUrl());
+
+        //if shop runs in https only mode we can set secure flag to all cookies
+        $blSecure = $blSecure || ($this->config->isSsl() && $this->config->getSslShopUrl() == $this->config->getShopUrl());
         return setcookie(
             $sName,
             $sValue,
